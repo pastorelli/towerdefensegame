2dec40d379b8fc6de9ac5ce6af247305625f5f4b
gpastorelli <gpastorelli89@gmail.com>
2017-03-14 18:19:19 -0400

    Add turrets on click

---
+++ TowerDefense/Assets/Scripts/BuildManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class BuildManager : MonoBehaviour {
+
+    public static BuildManager instance;
+
+    public GameObject standardTurretPrefab;
+
+    private GameObject turretToBuild;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("More than one BuildManager");
+        }
+        instance = this;
+    }
+
+    private void Start()
+    {
+        turretToBuild = standardTurretPrefab;
+    }
+
+    public GameObject GetTurretToBuild()
+    {
+        return turretToBuild;
+    }
+}
---
+++ TowerDefense/Assets/Scripts/Node.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Node : MonoBehaviour {
+
+    public Color hoverColor;
+    public Vector3 positionOffset;
+
+    private GameObject turret;
+
+    private Renderer rend;
+    private Color startColor;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+    }
+
+    private void OnMouseDown()
+    {
+        if (turret != null)
+        {
+            // TODO: Display on screen
+            Debug.Log("There is already a tower");
+        }
+
+        GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+        turret = (GameObject) Instantiate(turretToBuild, transform.position + positionOffset, transform.rotation);
+    }
+
+    private void OnMouseEnter()
+    {
+        rend.material.color = hoverColor;
+    }
+
+    private void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+}
